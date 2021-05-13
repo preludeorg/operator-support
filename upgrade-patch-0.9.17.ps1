a1e986c018e55662a91ba4caceb1e0e336aee2f6
privateducky <privateducky@icloud.com>
2021-05-13 16:40:31 -0400

    updates

--- upgrade-patch-0.9.17.ps1
+++
@@ -1,3 +0,0 @@
-Rename-Item $env:APPDATA\Operator\workspace $env:APPDATA\Operator\login.prelude.org;
-mv $env:APPDATA\Operator\settings.yml $env:APPDATA\Operator\login.prelude.org;
-mv $env:APPDATA\Operator\schedules.yml $env:APPDATA\Operator\login.prelude.org;
